83bb9a07e412d58992700e1958dbc25339bf8a5c
ari-jorgensen <32882891+ari-jorgensen@users.noreply.github.com>
2018-04-16 08:53:55 -0700

    Update receive.py

--- receive.py
+++ receive.py
@@ -5,13 +5,14 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
-# Create a message
+# Joining queue
 channel.queue_declare(queue='hello')
 
 # Function that handles received messages
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
+# Will wait for message in specified queue
 channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
